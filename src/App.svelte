fe82ba1c3d9cc5c75b8127be5e27439277433559
“MikaelaJ” <Mikaela_johansson@hotmail.com>
2021-02-09 21:51:46 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,79 @@
+<script>
+  import WorkList from "./components/WorkList.svelte";
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
+	import FrameWorks from "./components/FrameWorks.svelte";
+
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<link href="http://fonts.cdnfonts.com/css/neon-80s" rel="stylesheet">
+                
+                
+</svelte:head>
+
+<header>
+	<Header />
+</header>
+<main>
+	<WorkList />
+	<FrameWorks />
+</main>
+<footer>
+	<Footer />
+</footer>
+
+<style>
+  :global(:root) {
+		--bg: #202426;
+		--grey: #8c8c88;
+		--light: #f2f2f2;
+		--neonPink: #f24987ab;
+    font-family: 'Roboto', sans-serif;
+	}
+
+	:global(body) {
+		background: var(--bg);
+		background-image: url("/assets/img/noise-light.png");
+		color: var(--light);
+    width: inherit; 
+    min-height: inherit; 
+    margin: 0; 
+    padding: 0; 
+	}
+
+  :global(a) {
+    text-decoration: none;
+  }
+  :global(html) { 
+      width: 100%; 
+      min-height: 100%; 
+  } 
+  :global(*) { 
+    box-sizing: border-box; 
+} 
+ 
+ 
+/* body { 
+    width: inherit; 
+    min-height: inherit; 
+    margin: 0; 
+    padding: 0; 
+} */
+  
+  main {
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+
+	header {
+        margin: 0% 0% 5% 0%;
+	}
+  @media (max-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,105 @@
+<script>
+</script>
+
+<header>
+    <div class="container">
+        <h1>Frontend Developer</h1>
+        <h4>With a passion for patterns and problemsolving</h4>
+        <p>
+            For me, it is natural and important to seek knowledge and learn new
+            things. That is why I thrive in this industry, where new techniques
+            and effective working methods are constantly updated. My design
+            background in combination with technology has proven successful in
+            previous positions, where my technical interest together with my
+            creative side has been perceived as a strength. <br />
+            <strong class="pink-text">
+                I'm looking for new opportunities! <a
+                    href="mailto:mikaela@mikaelajohansson.com?subject=Found your site, just wanted to say hi! 😃"
+                    >Get in touch.</a
+                ></strong
+            >
+        </p>
+    </div>
+</header>
+
+<style>
+
+    h1 {
+        text-align: left;
+        font-family: 'Neon 80s', Arial, Helvetica, sans-serif;                          
+        color: var(--light);
+        padding: 0% 2rem 0% 0%;
+        text-transform: uppercase;
+        font-size: 5rem;
+        letter-spacing: 1.3rem;
+        line-height: 7rem;
+        text-shadow: 0 0 30px rgba(255, 0, 84, 0.6),
+            0 0 60px rgba(255, 0, 84, 0.4), 0 0 100px rgba(255, 0, 84, 0.2),
+            0 0 90px rgba(255, 0, 84, 0.1);
+    }
+    h4 {
+        margin: 0% 0% 1% 0%;
+        font-size: 1.7rem;
+        color: var(--neonPink);
+        letter-spacing: 0.11rem;
+        text-shadow: 0 0 30px rgba(255, 0, 84, 0.6),
+            0 0 60px rgba(255, 0, 84, 0.4), 0 0 100px rgba(255, 0, 84, 0.2),
+            0 0 90px rgba(255, 0, 84, 0.1);
+    }
+    p {
+        margin: auto 15% auto auto;
+        text-shadow: 1px 1px 30px rgba(0, 0, 0, 0.85);
+        letter-spacing: 0.1rem;
+        line-height: 1.4rem;
+    }
+    p .pink-text {
+        color: var(--neonPink);
+    }
+    a {
+        text-decoration: none;
+    }
+    a:hover {
+        text-decoration: none;
+    }
+    .container {
+        margin: auto;
+        width: 74vw;
+        height: 100%;
+        padding: 3% 3% 3% 3%;
+        color: #fff;
+        text-align: left;
+        text-shadow: 1px 1px 3px #333;
+        border-width: 7px;
+        border-style: outset;
+        border-color: #20242623;
+        background-image: linear-gradient(
+            135deg,
+            #8c8c8857 10%,
+            #20242675 100%
+        );
+    }
+    @media (max-width: 1024px) {
+        h1 {
+            font-size: 2.8rem;
+            letter-spacing: 0.8rem;
+        }
+        h4 {
+            font-size: 1.5rem;
+        }
+        p {
+            font-size: 1rem;
+        }
+    }
+    @media (max-width: 640px) {
+        h1 {
+            font-size: 3rem;
+            letter-spacing: 0.8rem;
+        }
+        h4 {
+            font-size: 1.3rem;
+        }
+        p {
+            font-size: 0.9rem;
+        }
+    }
+</style>
